e6a25a4cc38865b96be2f38fdc591ed69123c3f4
sreedevi111 <sreedevisobhana@gmail.com>
2022-06-14 10:32:05 +0530

    creating nav bar

--- amazon-clone/src/App.js
+++ amazon-clone/src/App.js
@@ -1,11 +1,18 @@
-import './App.css';
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import Home from "./components/Home";
 
 function App() {
   return (
-    <div className="App">
-      React App
+  <div className="App">
+    hiii
+    <Header />
+    <Routes>
+      <Route path="/" element ={<Home />} />
+    </Routes>
     </div>
-  );
+    );
 }
 
 export default App;
